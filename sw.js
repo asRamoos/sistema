7e83213e615728839c7208fd67dcdf4414c44af7
asRamoos <0001140501@senaimgaluno.com.br>
2026-03-24 14:53:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shadow-system-v8';
+const CACHE = 'shadow-system-v9';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './sw.js'];
 
 self.addEventListener('install', event => {
